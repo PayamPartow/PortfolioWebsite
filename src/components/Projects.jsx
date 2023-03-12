55974c8ed3d38ba542b63f8c768e1d33763a222f
PayamPartow <payampartow@icloud.com>
2023-03-11 18:36:32 -0800

    change to the first div
    md:h-screen to jusr h-screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,8 +9,8 @@ const Projects = () => {
     if (newWindow) newWindow.opener = null
   }
   return (
-    <div name= 'projects' className='w-full md:h-screen text-white bg-[#0a192f]'>
-      <div className='max-w-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div name= 'projects' className='w-full h-screen text-white bg-[#0a192f]'>
+      <div className='max-w-w[1000px] mx-auto p-12 flex flex-col justify-center w-full h-full'>
     <div className='pb-8'>
         <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
         <p className='py-6'>//Check out some of my recent Projects</p>
