9c0d8310f807c12858546e6fc02eeb4b12286151
PayamPartow <payampartow@icloud.com>
2023-03-22 22:15:24 -0700

    deleted md:h-screen from work
    deleted md:h-screen from work first div

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,7 +16,7 @@ import ReadmoreReadless from './ReadmoreReadless';
 const Work = () => {
     
   return (
-    <div name= 'work' className='w-full md:h-screen text-white bg-[#0a192f]'>
+    <div name= 'work' className='w-full  text-white bg-[#0a192f]'>
         <div className='max-w-w[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8 ml-10'>
             <p className='text-4xl font-bold ml-10 inline border-b-4 text-gray-300 border-pink-600'>Work Experience:</p>
